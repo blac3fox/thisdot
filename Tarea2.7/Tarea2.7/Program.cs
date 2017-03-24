ab0ce7cedb28e59c30ecfffea3a3e8e91ccac268
blac3fox <gladisarmenta@iMac-de-gladis.local>
2017-03-24 15:35:40 -0700

    Dados

--- Tarea2.7/Tarea2.7/Program.cs
+++ Tarea2.7/Tarea2.7/Program.cs
@@ -18,6 +18,11 @@ namespace Tarea2
 			Console.WriteLine("El valor del dado es " + valor);
 			Console.WriteLine("El color del dado es "+ color);
 		}
+
+		public void lanza(){
+			Random rng = new Random();
+			this.valor = rng.Next(1,13);
+		}
 		public static bool operator ==(Dado da1, Dado da2) {
 			if (da1.valor != da2.valor) { return false; } return true;
 		}
@@ -44,15 +49,25 @@ namespace Tarea2
 	{
 		public static void Main(string[] args)
 		{
+
 			Dado d1 = new Dado();
+			d1.lanza();
+			d1.Muestra();
 			Dado d2 = new Dado("Verde", 12);
+			d2.lanza();
+			d2.Muestra();
 			Dado d3 = new Dado("Rosa", 7);
+			d3.lanza();
+			d3.Muestra();
+			Dado mayor;
+			if (d1 == d2 || d2 == d3) { Console.WriteLine("Al menos 2 dados son iguales"); }
+			else { Console.WriteLine("Al menos 2 dado no son iguales");}
 
-			if (d1 == d2) { Console.WriteLine("Los 2 dados son iguales"); } else { Console.WriteLine("Los 2 dado no son iguales");}
+			if (d1 > d3) { mayor = d1; Console.WriteLine("Dado 1 es mayor a dado 3"); }
+			else { mayor = d3; Console.WriteLine("Dado 1  no es mayor a dado 3"); }
 
-			if (d1 > d3) { Console.WriteLine("Dado 1 es mayor a dado 3"); } else { Console.WriteLine("Dado 1  no es mayor a dado 3"); }
-
-			if (d3 < d2) { Console.WriteLine("Dado 2 es mayor a dado 3"); } else { Console.WriteLine("Dado 2 no es mayor a dado 3"); }
+			if (d2 > mayor) { mayor = d2; Console.WriteLine("Dado 2 es mayor a dado 3"); }
+			else { mayor = d3; Console.WriteLine("Dado 2 no es mayor a dado 3"); }
 
 
 
